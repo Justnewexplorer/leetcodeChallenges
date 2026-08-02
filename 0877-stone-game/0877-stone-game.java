fd58282eb067c58acb955df35c4e88141729add5
Adnan Ahmad <152091040+Justnewexplorer@users.noreply.github.com>
2026-08-02 16:30:36 +0530

    Time: 0 ms (100%), Space: 42.6 MB (97.64%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,18 +1,5 @@
 class Solution {
-
-    private int solve(int[] piles,int l,int r,Integer[][] dp){
-        if(l == r)
-            return piles[l];
-        if(dp[l][r] != null)
-            return dp[l][r];
-        int left = piles[l] - solve(piles,l+1,r,dp);
-        int right = piles[r] - solve(piles,l,r-1,dp);
-        dp[l][r] = Math.max(left,right);
-        return dp[l][r];        
-    }
-
     public boolean stoneGame(int[] piles) {
-        Integer[][] dp = new Integer[piles.length][piles.length];
-        return solve(piles,0,piles.length - 1,dp) > 0;
+        return true;
     }
 }
