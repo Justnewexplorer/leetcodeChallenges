a37526b209f90f759389a46cce0e0a8d28dd5dbb
Adnan Ahmad <152091040+Justnewexplorer@users.noreply.github.com>
2026-08-26 13:16:40 +0530

    Time: 1 ms (100%), Space: 44.2 MB (28.86%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
@@ -1,21 +1,34 @@
 class Solution {
     public String shortestBeautifulSubstring(String s, int k) {
+
+        int n = s.length();
+
+        int[] ones = new int[n];
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '1') {
+                ones[count++] = i;
+            }
+        }
+
+        if (count < k)
+            return "";
+
         String ans = "";
 
-        for(int i = 0; i < s.length(); i++){
-            int cnt = 0;
-            for(int j = i; j < s.length(); j++){
-                if(s.charAt(j) == '1')
-                    cnt++;    
-                if(cnt == k){
-                    String curr = s.substring(i , j + 1);
-                    if(ans.isEmpty() 
-                    || curr.length() < ans.length() 
-                    || curr.length() == ans.length() 
-                    && curr.compareTo(ans) < 0)
-                        ans = curr;
-                    break;
-                }    
+        for (int i = 0; i + k - 1 < count; i++) {
+            int left = ones[i];
+            int right = ones[i + k - 1];
+
+            String curr = s.substring(left, right + 1);
+
+            if (ans.isEmpty()
+                    || curr.length() < ans.length()
+                    || (curr.length() == ans.length()
+                        && curr.compareTo(ans) < 0)) {
+
+                ans = curr;
             }
         }
         return ans;
